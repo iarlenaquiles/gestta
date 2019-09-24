11d6c3f57b8ff1ef1495d2423fccb22ead287e01
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-09-24 17:28:05 -0300

    listando as tasks

--- src/app/controllers/TaskController.js
+++ src/app/controllers/TaskController.js
@@ -4,7 +4,19 @@ import Task from '../schemas/Task';
 
 class TaskController {
   async index(req, res) {
-    return res.json();
+    const tasks = await Task.find({}).sort({ status: 'asc' });
+    tasks.map(task => {
+      if (task.legal_date < new Date() && task.fine) {
+        task.status = 'FINE';
+      } else if (task.due_date < new Date()) {
+        task.status = 'OVERDUE';
+      } else {
+        task.status = 'OK';
+      }
+
+      task.save();
+    });
+    return res.json(tasks);
   }
 
   async store(req, res) {
